a6cbe67801df6e3208f47acb54e8e67e72e9e9cd
Mamokko <shalovlivaya.huliganka@gmail.com>
2024-09-20 17:32:05 +0500

    Redo the processing of a flat object into a nested one

--- src/getReport.js
+++ src/getReport.js
@@ -1,21 +1,27 @@
 const getIndentation = (depth, string = ' ', spaseCount = 2) => string.repeat(depth * spaseCount);
 
 const report = (data, formatName, depth = 1) => {
-  const result = data.flatMap((item) => {
-    const { name, type, value } = item;
+  const iter = (node, depth = 1) => {
+    const result = node.map((item) => {
+      const { name, type, value } = item;
+      switch (type) {
+        case 'nested': {
+          return `${getIndentation(depth)}  ${name}: {\n${iter(item.children, depth + 1)}\n${getIndentation(depth)}  }`;
+        }
+        case 'added':
+          return `${getIndentation(depth)}+ ${name}: ${value}`;
+        case 'deleted':
+          return `${getIndentation(depth)}- ${name}: ${value}`;
 
-    switch (type) {
-      case 'added':
-        return `${getIndentation(depth)}+ ${name}: ${value}`;
-      case 'deleted':
-        return `${getIndentation(depth)}- ${name}: ${value}`;
-      case 'changed':
-        return `${getIndentation(depth)}- ${name}: ${value.before}\n${getIndentation(depth)}+ ${name}: ${value.after}`;
-      default:
-        return `${getIndentation(depth)}  ${name}: ${value}`;
-    }
-  });
-  return `{\n${result.join('\n')}\n}`;
+        case 'changed':
+          return `${getIndentation(depth)}- ${name}: ${value.before}\n${getIndentation(depth)}+ ${name}: ${value.after}`;
+        default:
+          return `${getIndentation(depth)}  ${name}: ${value}`;
+      }
+    });
+    return result.join('\n');
+  };
+  return `{\n${iter(data)}\n}`;
 };
 
 export default report;
