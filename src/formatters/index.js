78c5739f5713d2cb745c9ae20708d07f0ba85f97
Mamokko <shalovlivaya.huliganka@gmail.com>
2024-09-30 14:54:44 +0500

    Add json formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -5,6 +5,8 @@ export default (data, formatName) => {
   switch (formatName) {
     case 'plain':
       return plain(data);
+    case 'json':
+      return JSON.stringify(data);
     default:
       return stylish(data);
   }
