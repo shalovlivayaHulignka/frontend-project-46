e7618797f1efb20ef6f3a3c73c72e6cc6e9c05d6
Mamokko <shalovlivaya.huliganka@gmail.com>
2024-09-30 14:11:03 +0500

    Edit tests for stylish and add tests for plain

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,8 +9,18 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('Default formater.', () => {
-  const expected = readFile('stylishFlatResult.txt');
-  const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file4.yaml'));
-  expect(actual).toEqual(expected);
+const fileResultStylish = readFile('result_stylish.txt');
+const fileResultPlain = readFile('result_plain.txt');
+
+const fileJsonOne = './__fixtures__/fileOne.json';
+const fileJsonTwo = './__fixtures__/fileTwo.json';
+const fileYamlOne = './__fixtures__/fileOne.yaml';
+const fileYamlTwo = './__fixtures__/fileTwo.yaml';
+
+describe('comparing  files', () => {
+  test('Formater.', () => {
+    expect(genDiff(fileYamlOne, fileYamlTwo)).toEqual(fileResultStylish);
+    expect(genDiff(fileYamlOne, fileYamlTwo, 'plain')).toEqual(fileResultPlain);
+  });
 });
+
